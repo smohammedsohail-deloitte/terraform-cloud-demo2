d4d1fc5ae993c69058a2ac4fc3e7506b1bd55073
Ubuntu <ubuntu@ip-172-31-38-145.ap-northeast-1.compute.internal>
2022-11-16 09:46:02 +0000

    added latest version

--- c3-security-groups.tf
+++ c3-security-groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "vpc-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["103.125.163.154/32"]
   }
   egress {
     description = "Allow all ip and ports outboun"
@@ -27,7 +27,7 @@ resource "aws_security_group" "vpc-web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["103.125.163.154/32"]
   }
 
   ingress {
@@ -35,7 +35,7 @@ resource "aws_security_group" "vpc-web" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["103.125.163.154/32"]
   }
 
   egress {
